c8e947e21432b63bfed569648b63ffa72d019e32
Dmytro <tsukanovdmytro@gmail.com>
2016-09-21 15:10:19 +0300

    Added blank UserController class

---
+++ app/controllers/user_controller.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+    User controller class for CRUD with User model.
+"""
+
+from app.models.User import User 
+
+class UserController(object):
+
+    """docstring for UserController"""
+    
+    pass
