2fd30c49b99e73736117ffcf6ffc467a5364f4bb
Denis Grebenets <dendendengrebenets@gmail.com>
2016-09-21 11:48:26 +0300

    Little changes

--- app/controllers/search_controller.py
+++ app/controllers/search_controller.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 import flask_whooshalchemy as wa
 
-from app import app
+import app
+
 from app.models.User import User
 
-app.config['SQLALCHEMY_DATABASE_URI']='mysql://denny:isurrender@localhost/local_db'
+app.app.config['SQLALCHEMY_DATABASE_URI']='mysql://denny:isurrender@localhost/local_db'
 
 wa.whoosh_index(app, User)
