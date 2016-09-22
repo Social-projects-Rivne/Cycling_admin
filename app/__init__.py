e1280fc20720119a6088ca914a0164bdef3a396d
Dmytro <tsukanovdmytro@gmail.com>
2016-09-22 16:26:11 +0300

    Reworked config.py due to new config.ini format v.3

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import sys, os
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+sys.path.append(os.path.abspath('../'))
+from config import DATABASE_URI
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URI
 db = SQLAlchemy(app)
 
 from app import urls, views, models
