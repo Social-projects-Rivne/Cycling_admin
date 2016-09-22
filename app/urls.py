4508c0eab01e1aacc0dbd58e2a0b99b78e280e06
OlexiiLykianchyk <olexii.lykianchyk@gmail.com>
2016-09-22 15:33:18 +0300

    Added route

--- app/urls.py
+++ app/urls.py
@@ -12,3 +12,12 @@ from app import app
 def hello():
     """Root entry point of application."""
     return "Hello World!"
+
+
+@app.route('/users/<id>/edit', methods=['GET', 'PUT'])
+def edit_user_page(id):
+    """
+    This method return edit user page
+    """
+    pass
+
