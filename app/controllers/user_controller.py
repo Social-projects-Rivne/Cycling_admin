3c809a3b8c5917b92173ec191a9836b9d443abd8
Dmytro <tsukanovdmytro@gmail.com>
2016-09-22 14:38:11 +0300

    1

--- app/controllers/user_controller.py
+++ app/controllers/user_controller.py
@@ -15,7 +15,7 @@ class AdminController(object):
     
     def delete_by_id(self, user_id):
         u_to_delete = User.query.filter_by(id=str(user_id)).first()
-        print u_to_delete.id
+        # print u_to_delete.id
         try:
             db.session.delete(u_to_delete)
             db.session.commit()
