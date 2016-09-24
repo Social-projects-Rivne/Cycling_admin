e0c3868faeb267c210def0e977dae8152f2d0f48
Denis Grebenets <dendendengrebenets@gmail.com>
2016-09-24 21:06:20 +0300

    Modified by Pep8

--- app/controllers/user_controller.py
+++ app/controllers/user_controller.py
@@ -5,8 +5,6 @@
     User controller class for CRUD with User model.
 """
 
-
-
 import cgi
 import sys, os
 
@@ -19,9 +17,9 @@ from app.views.view import View
 
 class AdminController(object):
     """docstring for AdminController"""
-    
+
     _admin_view = AdminView()
-    
+
     def __init__(self):
         """Create instances of models and views"""
         self.users_model = User()
@@ -48,21 +46,27 @@ class AdminController(object):
         return self.view.render_users_list(_users_list)
 
     def search_user(self, value):
-        exists = db.session.query(db.exists().where(User.full_name == value)).scalar()
-        exists2 = db.session.query(db.exists().where(User.email == value)).scalar()
-        exists3 = db.session.query(db.exists().where(User.role_id == value)).scalar()
-        if exists:   
-            result = db.session.query(User).filter_by(full_name = value).all()
+        """
+        Recieve from input, search for matches and return
+        dict of them if exists
+        """
+        exists = db.session.query(db.exists().
+                                  where(User.full_name == value)).scalar()
+        exists2 = db.session.query(db.exists().
+                                   where(User.email == value)).scalar()
+        exists3 = db.session.query(db.exists().
+                                   where(User.role_id == value)).scalar()
+        if exists:
+            result = db.session.query(User).filter_by(full_name=value).all()
             return self._admin_view.render_search_page(result)
         elif exists2:
-            result = db.session.query(User).filter_by(email = value).all()
+            result = db.session.query(User).filter_by(email=value).all()
             return self._admin_view.render_search_page(result)
         elif exists3:
-            result = db.session.query(User).filter_by(role_id = value).all()
+            result = db.session.query(User).filter_by(role_id=value).all()
             return self._admin_view.render_search_page(result)
         else:
-            return "Error input"
+            return self._admin_view.render_search_page("Matches doesn't exist")
 
 if __name__ == '__main__':
     pass
-
