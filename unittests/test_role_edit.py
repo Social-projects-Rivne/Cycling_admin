01a9fb88ee1982581a3849a3f1e6b5e01fb97914
Andrew Komar <0xporky@gmail.com>
2016-09-28 21:46:10 +0000

    Add unittest with Mock to change_user_role controller method.

--- unittests/test_role_edit.py
+++ unittests/test_role_edit.py
@@ -11,6 +11,7 @@ that quick changes user role, and method that quick get user role.
 from unittest import TestCase
 from unittest import main
 from mock import patch
+from mock import Mock
 from json import loads
 
 from app.controllers.user_controller import AdminController
@@ -22,12 +23,27 @@ class TestRoleEdit(TestCase):
 
     def test_get_user_by_id(self):
         """Test user controllier method that returns user role by id."""
-        with patch('app.controllers.user_controller.User') as mock:
-            mock.query.filter_by.return_value.first.return_value.role.return_value = u'admin'
+        with patch('app.controllers.user_controller.User') as mock_model:
+            mock_model.query.filter_by.return_value.\
+                first.return_value.\
+                role.return_value = u'admin'
+
             c = AdminController()
             result = c.get_user_role_by_id(1)
             message = loads(result[0])
             self.assertEqual(message['message'], u'admin')
+
+    def test_change_user_group(self):
+        with patch('app.controllers.user_controller.User') as mock_model:
+            mock_model.query.filter_by.return_value.\
+                first.return_value.\
+                role_id = 0
+
+            c = AdminController()
+            c.is_last_admin = Mock(return_value=False)
+            result = c.change_user_group(1, {"user_role": 1})
+            message = loads(result[0])
+            self.assertEqual(message['message'], u'OK')
 
 
 if __name__ == '__main__':
