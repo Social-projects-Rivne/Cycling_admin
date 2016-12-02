28951fd1b0445999c675ad9ca5ceed9a323326a0
barbarossa22 <moodaq@gmail.com>
2016-12-02 13:46:53 +0200

    Fix the issue caused by database returning unicode string instead of integer for role_id column.

--- app/models/user.py
+++ app/models/user.py
@@ -40,7 +40,7 @@ class User(db.Model, UserMixin):
     roles = ['user', 'admin']
 
     def role(self):
-        return self.roles[self.role_id]
+        return self.roles[int(self.role_id)]
 
     def __repr__(self):
         return '<User %s>' % self.full_name
